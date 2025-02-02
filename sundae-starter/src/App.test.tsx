277749c37df4a696ae8bde43b35c18f93128b199
HsiehChingHao <rickypic0521@gmail.com>
2025-02-02 23:49:56 +0800

    updated:2/2 queryLearning

--- sundae-starter/src/App.test.tsx
+++ sundae-starter/src/App.test.tsx
@@ -1,22 +1,54 @@
 import React from "react"; // 顯式導入 React
-import { render, screen, fireEvent } from "@testing-library/react";
+import { logRoles, render, waitFor, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "./summary/SummaryForm";
 
 test("test start", async () => {
+  //setup user
+  const user = userEvent.setup();
   render(<SummaryForm />);
   const chocolate = screen.getByRole("checkbox", { name: "Chocolate" });
-  const strawberry = screen.getByRole("checkbox", { name: "Strawberry" });
   const btn = screen.getByRole("button");
   expect(chocolate).toBeInTheDocument();
-  expect(strawberry).toBeInTheDocument();
+
   //預設不選chkBox
   expect(chocolate).not.toBeChecked();
-  expect(strawberry).not.toBeChecked();
   expect(btn).not.toBeEnabled();
+
   //選了 chkBox 可點 btn
-  fireEvent.click(chocolate);
+  await user.click(chocolate);
   expect(btn).toBeEnabled();
   //取消 chkBox 不可點 btn
-  fireEvent.click(chocolate);
+  await user.click(chocolate);
   expect(btn).not.toBeEnabled();
+});
+
+test("popover test", async () => {
+  // 設定 userEvent
+  const user = userEvent.setup();
+
+  // 取得 container
+  const { container } = render(<SummaryForm />);
+
+  // logRoles 主要功用：打印出渲染元素內部有 role 的元素
+  logRoles(container);
+
+  // 取得 label 標籤 (getByRole / getByLabelText 等表單元素也沒辦法使用，所以用 getByText)
+  const label = screen.getByText(/chocolate/i);
+
+  // 確保元素在 document
+  expect(label).toBeInTheDocument();
+
+  // 取得 hover 後才會出現的元素（一開始不在文件內）
+  // 只有使用 queryBy... 才不會因為找不到元素 throw err
+  // 錯誤：const tooltip = screen.getByRole("tooltip");
+  const tooltip = screen.queryByTestId("tooltip");
+  expect(tooltip).not.toBeInTheDocument();
+
+  // 確認使用者行為執行後，確認結構產生變化
+  // user 為 promise 必須使用 await
+  // expect(tooltip) 的話，在一開始就已經因為找不到所以為 null
+  await user.hover(label);
+  logRoles(container);
+  expect(screen.getByTestId("tooltip")).toBeInTheDocument();
 });
--- sundae-starter/src/summary/SummaryForm.tsx
+++ sundae-starter/src/summary/SummaryForm.tsx
@@ -4,24 +4,29 @@ import "./SummaryForm.scss";
 export default function SummaryForm() {
   const [isSelected, setIsSelected] = useState(false);
   const [checkChocolate, setCheckChocolate] = useState(false);
-  const [checkStraw, setCheckStraw] = useState(false);
+  const [isHover, setIsHover] = useState(false);
   const handleSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.id === "chocolate") {
       setCheckChocolate((prev) => !prev);
     }
-    if (e.target.id === "strawberry") {
-      setCheckStraw((prev) => !prev);
-    }
   };
 
   useEffect(() => {
-    if (!checkChocolate && !checkStraw) {
+    if (!checkChocolate) {
       setIsSelected(false);
     }
-    if (checkChocolate || checkStraw) {
+    if (checkChocolate) {
       setIsSelected(true);
     }
-  }, [checkChocolate, checkStraw]);
+  }, [checkChocolate]);
+
+  const handleMouseEnter = (e: React.MouseEvent<HTMLElement>) => {
+    setIsHover(true);
+  };
+  const handleMouseLeave = (e: React.MouseEvent<HTMLElement>) => {
+    setIsHover(false);
+  };
+
   return (
     <>
       <div className="form">
@@ -33,17 +38,22 @@ export default function SummaryForm() {
             checked={checkChocolate}
             onChange={handleSelect}
           />
-          <label htmlFor="chocolate">Chocolate</label>
-        </div>
-        <div className="form_item">
-          <input
-            className="me-3"
-            type="checkbox"
-            id="strawberry"
-            checked={checkStraw}
-            onChange={handleSelect}
-          />
-          <label htmlFor="strawberry">Strawberry</label>
+          <label
+            htmlFor="chocolate"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            Chocolate
+          </label>
+          {isHover && (
+            <div
+              data-testid="tooltip"
+              role="tooltip" // 加上角色 logRoles 才會顯示
+              className="item-tooltip mx-3 item-tooltip_active"
+            >
+              巧克力
+            </div>
+          )}
         </div>
         <button className="submit-btn" disabled={!isSelected}>
           Submit!
